709300fd5612312d0314628e2c52878250af86a9
Sulagna Pradhan <152047288+Sulagna-pradhan@users.noreply.github.com>
2024-04-13 16:25:17 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-# app.py
-
 from flask import Flask, render_template, request, redirect, url_for
-from pytube import YouTube
-from pytube.exceptions import VideoUnavailable
+import youtube_dl
 
 app = Flask(__name__)
 
@@ -13,26 +10,25 @@ def index():
 @app.route('/download', methods=['POST'])
 def download():
     video_link = request.form['video_link']
-    
-    try:
-        yt = YouTube(video_link)
-        streams = yt.streams.filter(progressive=True)
-        return render_template('download.html', streams=streams)
-    except VideoUnavailable:
-        return "Video is unavailable. Please try another video link."
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': 'downloads/%(title)s.%(ext)s',  # Output template for the downloaded file
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(video_link, download=False)
+        title = info_dict.get('title', 'Video')
+    return render_template('download.html', title=title)
 
 @app.route('/download_video', methods=['POST'])
 def download_video():
     video_link = request.form['video_link']
-    selected_stream = request.form['selected_stream']
-    
-    try:
-        yt = YouTube(video_link)
-        stream = yt.streams.get_by_itag(selected_stream)
-        stream.download()
-        return redirect(url_for('index'))
-    except VideoUnavailable:
-        return "Video is unavailable. Please try another video link."
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': 'downloads/%(title)s.%(ext)s',  # Output template for the downloaded file
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_link])
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
